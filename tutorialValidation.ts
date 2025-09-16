2248d97bd554160819897c64ce7bcd69e8d69dad
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2025-09-16 16:41:57 -0700

    Initial files for MakeCode project

---
+++ tutorialValidation.ts
@@ -0,0 +1,26 @@
+
+//% block="Tutorial Controls"
+//% icon="\uf0e0"
+//% color="#378273"
+namespace tutorialcontrols {
+
+    // Must match MakeCode internal representation
+    interface TutorialValidationResult {
+        isValid: boolean;
+        message?: string;
+    }
+
+    //% block="send tutorial validion result isValid $isValid ||with $message"
+    //% blockId=tcsendvalidationresult
+    //% weight=50
+    export function sendValidationResult(isValid: boolean, message: string) {
+        const data: TutorialValidationResult = { isValid, message };
+        const toSend = JSON.stringify(data);
+        const buf = Buffer.create(toSend.length);
+        for (let i = 0; i < toSend.length; ++i) {
+            buf[i] = toSend.charCodeAt(i);
+        }
+
+        control.simmessages.send("tutorialcodevalidation", buf, true /** noBroadcast */);
+    }
+}
